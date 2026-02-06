f50de930a752cdc1f82981e12f5145f7134569c6
Antoine Clarman <antoinereims28@gmail.com>
2026-02-06 16:16:30 +0100

    feat: add day1 part2

---
+++ day1/src/bin/part2.rs
@@ -0,0 +1,25 @@
+use std::io::{self, Read};
+
+use day1::solve_part_2;
+
+fn parse_str(str: &str) -> Vec<String> {
+    let mut parsed_str = vec![];
+
+    for word in str.split_whitespace() {
+        parsed_str.push(word.to_string());
+    }
+    parsed_str
+}
+
+fn get_input() -> Vec<String> {
+    let mut buf = String::new();
+    let _ = io::stdin().read_to_string(&mut buf);
+
+    parse_str(&buf)
+}
+
+fn main() {
+    let input = get_input();
+    let result = solve_part_2(input.iter().map(std::string::String::as_str).collect());
+    println!("{result}");
+}
--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum Rotation {
     Left(u64),
@@ -52,6 +54,52 @@ pub fn solve_part_1(input: Vec<&str>) -> u64 {
     result
 }
 
+#[must_use]
+pub fn solve_part_2(input: Vec<&str>) -> u64 {
+    let mut rotations = vec![];
+    for rotation in input {
+        if let Some(rotation) = convert_rotation(rotation) {
+            rotations.push(rotation);
+        }
+    }
+    let mut result = 0;
+    let mut dial = 50;
+    for rotation in rotations {
+        match rotation {
+            Rotation::Left(rotate) => {
+                result += rotate / 100;
+                let rotate = rotate % 100;
+                if rotate > 0 {
+                    match dial.cmp(&rotate) {
+                        Ordering::Equal => {
+                            result += 1;
+                            dial = 0;
+                        }
+                        Ordering::Less => {
+                            if dial != 0 {
+                                result += 1;
+                            }
+                            dial += 100 - rotate;
+                        }
+                        Ordering::Greater => {
+                            dial -= rotate;
+                        }
+                    }
+                }
+            }
+            Rotation::Right(rotate) => {
+                result += rotate / 100;
+                let rotate = rotate % 100;
+                let diff = dial + rotate;
+                result += diff / 100;
+                dial = diff % 100;
+            }
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,6 +111,68 @@ mod tests {
         ];
         let result = solve_part_1(input);
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn example_solve_part_2() {
+        let input = vec!["L68"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 1);
+        let input = vec!["L68", "L30"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 1);
+        let input = vec!["L68", "L30", "R48"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 2);
+        let input = vec!["L68", "L30", "R48", "L5"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 2);
+        let input = vec!["L68", "L30", "R48", "L5", "R60"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 3);
+        let input = vec!["L68", "L30", "R48", "L5", "R60", "L55"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 4);
+        let input = vec!["L68", "L30", "R48", "L5", "R60", "L55", "L1"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 4);
+        let input = vec!["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 5);
+        let input = vec!["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 5);
+        let input = vec![
+            "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82",
+        ];
+        let result = solve_part_2(input);
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn left_limit_solve_part_2() {
+        let input = vec!["L1000"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 10);
+        let input = vec!["L100"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 1);
+        let input = vec!["L50"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn right_limit_solve_part_2() {
+        let input = vec!["R1000"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 10);
+        let input = vec!["R100"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 1);
+        let input = vec!["R50"];
+        let result = solve_part_2(input);
+        assert_eq!(result, 1);
     }
 
     #[test]
